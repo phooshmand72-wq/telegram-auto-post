7c595e99477851c7d5f7308325531607feeed82a
phooshmand72-wq <phooshmand72@gmail.com>
2026-08-04 23:08:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+import json
 
 url = "https://hooshmandyadak.ir/products?ordering=NEWEST"
 
@@ -7,4 +9,28 @@ r = requests.get(
     headers={"User-Agent": "Mozilla/5.0"}
 )
 
-print(r.text[:5000])
+soup = BeautifulSoup(r.text, "html.parser")
+
+next_data = soup.find(
+    "script",
+    id="__NEXT_DATA__"
+)
+
+if next_data:
+    print("NEXT DATA FOUND")
+
+    data = json.loads(
+        next_data.text
+    )
+
+    print(data.keys())
+
+    print(
+        json.dumps(
+            data,
+            ensure_ascii=False
+        )[:3000]
+    )
+
+else:
+    print("NO NEXT DATA")
