42162e8923146404eb499c5acd2f4e2fe8d319ea
phooshmand72-wq <phooshmand72@gmail.com>
2026-08-04 23:26:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import requests
-from bs4 import BeautifulSoup
+import re
 
-url = "https://hooshmandyadak.ir/products?ordering=NEWEST"
+url = "https://digifycdn.com/front-statics/digitheme/production/_next/static/chunks/pages/products/%5B%5B...categoryNames%5D%5D-88e4f3df83f780d8.js"
 
-r = requests.get(
-    url,
-    headers={"User-Agent": "Mozilla/5.0"}
-)
+r = requests.get(url)
 
-soup = BeautifulSoup(r.text, "html.parser")
+text = r.text
 
-for script in soup.find_all("script", src=True):
-    src = script["src"]
-    print(src)
+print("LENGTH:", len(text))
+
+for word in ["product", "products", "api", "queryKey", "getProducts"]:
+    print(word, text.find(word))
+
+# قسمت‌هایی که api دارند
+for match in re.findall(r'.{0,80}api.{0,120}', text):
+    print("----")
+    print(match)
