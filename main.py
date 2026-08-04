e4557f20ce5ff3e4ec9089f57f7ebb8d2dedacab
phooshmand72-wq <phooshmand72@gmail.com>
2026-08-04 23:24:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import json
 
 url = "https://hooshmandyadak.ir/products?ordering=NEWEST"
 
@@ -11,16 +10,6 @@ r = requests.get(
 
 soup = BeautifulSoup(r.text, "html.parser")
 
-data = json.loads(
-    soup.find("script", id="__NEXT_DATA__").text
-)
-
-queries = data["props"]["dehydratedState"]["queries"]
-
-print("QUERY COUNT:", len(queries))
-
-for q in queries:
-    print("----------------")
-    print(q.keys())
-    print("QUERY KEY:")
-    print(q.get("queryKey"))
+for script in soup.find_all("script", src=True):
+    src = script["src"]
+    print(src)
