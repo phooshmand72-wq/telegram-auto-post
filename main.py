6b4c44d65d048d912e5654dda8f8753e874e4c38
phooshmand72-wq <phooshmand72@gmail.com>
2026-08-04 23:01:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-urls = [
-    "https://hooshmandyadak.ir/products?ordering=NEWEST",
-    "https://hooshmandyadak.ir/blogs"
-]
+url = "https://hooshmandyadak.ir/products?ordering=NEWEST"
 
-for url in urls:
-    print("\nURL:", url)
+r = requests.get(
+    url,
+    headers={"User-Agent": "Mozilla/5.0"}
+)
 
-    r = requests.get(
-        url,
-        headers={"User-Agent": "Mozilla/5.0"}
-    )
+soup = BeautifulSoup(r.text, "html.parser")
 
-    print("STATUS:", r.status_code)
+for a in soup.find_all("a", href=True):
+    href = a["href"]
 
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    print("TITLE:", soup.title.text)
-
-    for a in soup.find_all("a", href=True)[:20]:
-        print(a.get_text(strip=True), a["href"])
+    if "/product/" in href:
+        print(
+            a.get_text(strip=True),
+            href
+        )
