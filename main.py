1a897026f360cb69c874d0f29be0a90bb9fbbf4c
phooshmand72-wq <phooshmand72@gmail.com>
2026-08-04 22:58:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-raise Exception("NEW MAIN FILE TEST")
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,10 +14,11 @@ for url in urls:
         headers={"User-Agent": "Mozilla/5.0"}
     )
 
+    print("STATUS:", r.status_code)
+
     soup = BeautifulSoup(r.text, "html.parser")
 
     print("TITLE:", soup.title.text)
 
     for a in soup.find_all("a", href=True)[:20]:
         print(a.get_text(strip=True), a["href"])
-print("TEST HTML STARTED")
