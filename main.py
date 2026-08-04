ee476577cd61d9363cffb403961999d3147c356d
phooshmand72-wq <phooshmand72@gmail.com>
2026-08-04 22:36:06 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,22 @@
-import os
 import requests
+from bs4 import BeautifulSoup
 
-SITE_URL = "https://hooshmandyadak.ir"
+urls = [
+    "https://hooshmandyadak.ir/products?ordering=NEWEST",
+    "https://hooshmandyadak.ir/blogs"
+]
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHANNEL_ID = "@hooshmandYadak"
+for url in urls:
+    print("\nURL:", url)
 
-
-def send_telegram():
-
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-
-    message = """
-✅ تست اتصال هوشمند یدک
-
-ربات تلگرام با موفقیت به GitHub Actions متصل شد.
-
-🔧 هوشمند یدک
-🌐 hooshmandyadak.ir
-
-#هوشمند_یدک
-#لوازم_یدکی_خودرو
-"""
-
-    keyboard = {
-        "inline_keyboard": [
-            [
-                {
-                    "text": "🔗 مشاهده در سایت",
-                    "url": SITE_URL
-                }
-            ]
-        ]
-    }
-
-    data = {
-        "chat_id": CHANNEL_ID,
-        "text": message,
-        "reply_markup": keyboard,
-        "parse_mode": "HTML"
-    }
-
-    response = requests.post(
+    r = requests.get(
         url,
-        data=data
+        headers={"User-Agent": "Mozilla/5.0"}
     )
 
-    print(response.text)
+    soup = BeautifulSoup(r.text, "html.parser")
 
+    print("TITLE:", soup.title.text)
 
-if __name__ == "__main__":
-    send_telegram()
+    for a in soup.find_all("a", href=True)[:20]:
+        print(a.get_text(strip=True), a["href"])
